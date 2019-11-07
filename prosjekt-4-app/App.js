a21363b502697e88be214609e12d4f913548e1c8
Kristoffer <peders92@hotmail.com>
2019-11-07 13:33:56 +0100

    #1 Add searchField and DisplaySection

--- prosjekt-4-app/App.js
+++ prosjekt-4-app/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './src/store';
+import DisplaySection from './src/components/organisms/DisplaySection';
 
 export default function App() {
   return (
     <Provider store={store}>
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <DisplaySection/>
       </View>
     </Provider>
   );
