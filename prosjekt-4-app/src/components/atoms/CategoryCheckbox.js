543e2784bdd908c7153fb87c4ae5144b38e348bc
Kristoffer <peders92@hotmail.com>
2019-11-10 15:53:50 +0100

    #7 #10 Fix checkboxes and add styling

--- prosjekt-4-app/src/components/atoms/CategoryCheckbox.js
+++ prosjekt-4-app/src/components/atoms/CategoryCheckbox.js
@@ -1,31 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { checkCategory, uncheckCategory }  from "../../state/actions/categoryActions";
 import { connect } from 'react-redux'
-import { searchForItem }  from "../../state/actions/searchActions";
 import { CheckBox } from 'react-native-elements'
-import { Button, TextInput, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 //Checkboxes to select categories of menu items, uses two actions for checking and unchecking,
 //as well as one for updating the searchresults
-const CategoryCheckbox = ({ categoryShown, categoryQuery, checkCategory, uncheckCategory, searchForItem, searchWord }) => {
+const CategoryCheckbox = ({ categoryShown, categoryQuery, checkCategory, uncheckCategory }) => {
     const [ checked, setChecked ] = useState(false);
     
-    const handleChange = () => {
-        setChecked(!checked)
-        console.log(checked)
-        if(checked) {
-            uncheckCategory(categoryQuery);
-        } else {
-            checkCategory(categoryQuery);
-        }
-        searchForItem(searchWord)
-  };
+    useEffect(() => {
+      if(checked) {
+        checkCategory(categoryQuery);
+      } else {
+        uncheckCategory(categoryQuery);
+      }
+    });
+
     return(
       <View>
         <CheckBox
         title={categoryShown}
         checked={checked}
-        onPress={() => handleChange()}
+        onPress={() => setChecked(!checked)}
+        size={24}
+        checkedColor={'#FF6A6A'}
+        uncheckedColor={'#43484D'}
+        containerStyle={styles.checkbox}
         />
       </View>
     );
@@ -34,14 +35,17 @@ const CategoryCheckbox = ({ categoryShown, categoryQuery, checkCategory, uncheck
 //Dispatching actions to the store
 const mapDispatchToProps = {
   checkCategory,
-  uncheckCategory,
-  searchForItem
+  uncheckCategory
  };
 
-//Extract data from the store
-const mapStateToProps = (state) => ({
-  searchWord: state.searchWord
-});
-
 //Connect the store to this component
-export default connect(mapStateToProps, mapDispatchToProps)(CategoryCheckbox)
+export default connect(null, mapDispatchToProps)(CategoryCheckbox)
+
+//Styling
+const styles = StyleSheet.create({
+  checkbox: {
+    backgroundColor: '#ffe836',
+    borderWidth: 0,
+    padding: 0,
+  }
+});
